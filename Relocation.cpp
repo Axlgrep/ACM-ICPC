c5e931deb9b140ee6e60569bc4fe3e4bb1454ba2
Axlgrep <wuxianjian@vip.qq.com>
2015-08-18 09:47:22 +0800

    update

--- Relocation.cpp
+++ Relocation.cpp
@@ -17,8 +17,8 @@
 #define Prime 999983
 #define N 10 
 using namespace std;
-int c1, c2, n, w[N];
-int dp1[1<<N], dp2[1<<N], dp3[1<<N];
+int c1, c2, n, ans, w[N];
+int dp1[1<<N], dp2[1<<N];
 
 int getwei(int state)
 {
@@ -32,28 +32,27 @@ int getwei(int state)
 void DP()
 {
      int tot;
-     dp1[0] = dp2[0] = dp3[0] = 0;
-
+     dp1[0] = dp2[0] = 0;
+     ans = INF;
      for(int i = 0; i < (1<<n); i++)
      {
            tot = getwei(i);
-           if(tot > 0 && tot <= c1)
+           if(tot <= c1)
                   dp1[i] = 1;
            else
-                  for(int j = 0; j < (1<<n); j++)
-                         if( (i | j) == i && getwei(i ^ j) <= c1)
-                                dp1[i] = min(dp1[i], dp1[j] + 1);
+                  for(int j = 0; j < i; j++)
+                         dp1[i] = min(dp1[i], dp1[i ^ j] + dp1[j]);
 
-           if(tot > 0 && tot <= c2)
+           if(tot <= c2)
                   dp2[i] = 1;
            else
-                  for(int j = 0; j < (1<<n); j++)
-                         if( (i | j) == i && getwei(i ^ j) <= c2)
-                                dp2[i] = min(dp2[i], dp2[j] + 1);
+                  for(int j = 0; j < i; j++)
+                         dp2[i] = min(dp2[i], dp2[i ^ j] + dp2[j]);
      }
      for(int i = 0; i < (1<<n); i++)
         for(int j = 0; j < (1<<n); j++)
-               dp3[i | j] = min(dp3[i | j], max(dp1[i], dp2[j]));
+              if( (i | j) == (1<<n) - 1)
+                      ans = min(ans, max(dp1[i], dp2[j]));
 }
 
 int main()
@@ -71,12 +70,10 @@ int main()
       
        memset(dp1, 9999, sizeof(dp1));
        memset(dp2, 9999, sizeof(dp2));
-       memset(dp3, 9999, sizeof(dp3));
 
        DP();
-       printf("Scenario #%d:\n%d\n\n",t++,  dp3[(1<<n) - 1]);       
+       printf("Scenario #%d:\n%d\n\n",t++, ans);       
    }
    return 0;
-  
 }
 
